33da7b5eb9d7b8ad8c230640d3e7bc4f3090dfb3
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2023-03-31 16:11:51 -0400

    .

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 					options: {
 						presets: [
 							['@babel/preset-env', { targets: "defaults" }],
-							['@babel/preset-react', { targets: "defaults" }],
+							'@babel/preset-react'
 						]
 					}
 				}
